7c7a7e77eab6c9fdbaa9a42141058ccda3b03abf
Peter Janku <janku@utb.cz>
2022-10-10 22:49:18 +0200

    Improved demonstration in main function.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "point.h"
 #include "rectangle.h"
+#include "circle.h"
 
 
 int main() {
     ShapeLib::Point p1(0, 0);
 
-    ShapeLib::Rectangle r(0,0,0,10,20);
-    r.printInfo();
-    std::cout << "H:"<< r.getHeight() << " W:" << r.getWidth() << std::endl;
+    ShapeLib::Rectangle r(0, 0, 0, 10, 20);
     std::cout << r;
+
+    ShapeLib::Circle c(1, 0, 0, 10);
+    std::cout << c;
     return 0;
 }
